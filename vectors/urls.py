7f0ba777f27dceca984bbbcffb824e7c3c81ca67
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-07-22 15:38:18 +0100

    ex 5 finished

--- vectors/urls.py
+++ vectors/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, ex1
+from .views import home, ex1, ex5
 
 urlpatterns = [
     path('', home, name='home'),
     path('ex1', ex1, name='ex1_x'),
+    path('ex5', ex5, name='ex5_x'),
 
 ]
--- vectors/views.py
+++ vectors/views.py
@@ -27,3 +27,26 @@ def ex1(request):
     return render(request, 'ex1.html', {'list_value': list_value,
                                         'x': x, 'y': y, 'x_p': x_p,
                                         'y_p': y_p, 'sum': sum})
+
+
+def ex5(request):
+    vet = []
+    vet_list = []
+    tot_par = 0
+    par = []
+    value = None
+
+    if request.method == 'POST':
+        value = request.POST.get('vector_x')
+        vet = value.split(",")
+
+    for i in vet:
+        vet_list.append(int(i))
+
+    for i in vet_list:
+        if i % 2 == 0:
+            tot_par += 1
+            par.append(i)
+
+    tot_par = tot_par
+    return render(request, 'ex5.html', {'tot_par': tot_par, 'value': value, 'par': par})
