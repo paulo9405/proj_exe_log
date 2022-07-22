80ed9e3c84785f77e43f806f65cd8ba1e9b025e6
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-07-22 14:44:15 +0100

    ex1

--- vectors/views.py
+++ vectors/views.py
@@ -1,2 +1,29 @@
 from django.shortcuts import render
 
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def ex1(request):
+    list_value = None
+    x = None
+    y = None
+    x_p = None
+    y_p = None
+    sum = None
+
+    if request.method == "POST":
+        value_1 = request.POST.get('value_1')
+        list_value = value_1.split(",")
+
+        x = int(request.POST.get('value_x'))
+        y = int(request.POST.get('value_y'))
+
+        x_p = list_value[x]
+        y_p = list_value[y]
+        sum = int(list_value[x]) + int(list_value[y])
+
+    return render(request, 'ex1.html', {'list_value': list_value,
+                                        'x': x, 'y': y, 'x_p': x_p,
+                                        'y_p': y_p, 'sum': sum})
