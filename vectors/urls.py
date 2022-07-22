3201911ce8552e9bcd33d827f00ceb45229937ac
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-07-22 15:52:09 +0100

    organized

--- vectors/urls.py
+++ vectors/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import home, ex1, ex5
+from .views import home, ex1, ex2
 
 urlpatterns = [
     path('', home, name='home'),
     path('ex1', ex1, name='ex1_x'),
-    path('ex5', ex5, name='ex5_x'),
+    path('ex5', ex2, name='ex2_x'),
 
 ]
