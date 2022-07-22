84e91697a240c0029eeb459e45bc3025ca675bc5
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-07-22 15:31:24 +0100

    ex 4 finished

--- vectors/urls.py
+++ vectors/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, ex1
+from .views import home, ex1, ex4
 
 urlpatterns = [
     path('', home, name='home'),
     path('ex1', ex1, name='ex1_x'),
+    path('ex4', ex4, name='ex4_x'),
 
 ]
--- vectors/views.py
+++ vectors/views.py
@@ -27,3 +27,25 @@ def ex1(request):
     return render(request, 'ex1.html', {'list_value': list_value,
                                         'x': x, 'y': y, 'x_p': x_p,
                                         'y_p': y_p, 'sum': sum})
+
+
+def ex4(request):
+    vet = []
+    vet_list = []
+    in_vet = None
+    value_x = None
+    if request.method == 'POST':
+        value = request.POST.get('vector_1')
+        vet = value.split(",")
+        value_x = request.POST.get('value_x')
+
+        for i in vet:
+            c = i.replace(' ', '')
+            vet_list.append(c)
+
+    if value_x not in vet_list:
+        not_in = 'Value not in vector.'
+        return render(request, 'ex4.html', {'not_in': not_in})
+
+    in_vet = vet_list.index(value_x)
+    return render(request, 'ex4.html', {'in_vet': in_vet, 'vet_list': vet_list, 'value_x': value_x})
