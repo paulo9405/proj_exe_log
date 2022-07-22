0f8405e315ccc38797d29a72e1f3842bfaeb4461
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-07-22 16:24:16 +0100

    Finished ex5 correct

--- vectors/urls.py
+++ vectors/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import home, ex1, ex2, ex3, ex4
+from .views import home, ex1, ex2, ex3, ex4, ex5
 
 urlpatterns = [
     path('', home, name='home'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('ex2', ex2, name='ex2_x'),
     path('ex3', ex3, name='ex3_x'),
     path('ex4', ex4, name='ex4_x'),
+    path('ex5', ex5, name='ex5_x'),
 
 ]
