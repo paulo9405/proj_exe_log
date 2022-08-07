28276c6e4e5c968b876490d6ee69fa52ac7fae53
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-08-07 21:40:47 +0100

    ex5 matrices finished

--- matrices/views.py
+++ matrices/views.py
@@ -35,3 +35,45 @@ def matrice_ex1(request):
                 position = i, j
 
     return render(request, 'matrices/matrice_ex1.html', {'matrices': matrices, 'maior': maior, 'position': position})
+
+
+def matrice_ex5(request):
+    if request.method != 'POST':
+        return render(request, 'matrices/matrice_ex5.html')
+
+    value = request.POST.get('value1').split(',')
+
+    if len(value) != 5:
+        error = 'Plaese write just 10 values.'
+        return render(request, 'matrices/matrice_ex5.html', {'error': error})
+
+    integers = [int(x) for x in value]
+    valid = [x >= 10 for x in integers]
+
+    if (not all(valid)):
+        error = 'Plaese write values grater than 9.'
+        return render(request, 'matrices/matrice_ex5.html', {'error': error})
+
+    matrices = []
+    for x in integers:
+        matrices.append([random.randint(1, x) for _ in range(5)])
+
+    value2 = request.POST.get('value2')
+    value2_int = int(value2)
+
+    positions = []
+    for i in range(0, len(matrices)):
+        for j in range(0, len(matrices)):
+            if matrices[i][j] == value2_int:
+                position = i, j
+                positions.append(position)
+
+    for l in matrices:
+        for c in l:
+            if value2_int == c:
+                return render(request, 'matrices/matrice_ex5.html', {
+                    'matrices': matrices, 'positions': positions, 'value2_int': value2_int})
+
+    error2 = 'not in matrice.'
+    return render(request, 'matrices/matrice_ex5.html', {'matrices': matrices, 'error2': error2, 'value2_int': value2_int})
+
--- vectors/tests.py
+++ vectors/tests.py
@@ -1,37 +1,57 @@
-
-# for i in a:
-#     b.append(random.sample(range(0, int(i)), 5))
 #
-# print(b)
+# # for i in a:
+# #     b.append(random.sample(range(0, int(i)), 5))
+# #
+# # print(b)
+#
+# a = '10, 4, 70, 80, 50, 100, 150, 20, 45, 88'
+#
+#
+# def test1(value):
+#     print(value)
+#     arr = value.split(',')
+#     print(arr)
+#     integers = [int(x) for x in arr]
+#     print(integers)
+#     valid = [x >= 10 for x in integers] # retorna TRUE OU FALSE para cada iten do array
+#     print(valid)
+#
+#     if (all(valid)): # varifica se todos os itens do array sao True
+#         print('All values are valid!')
+#     if (not all(valid)): # oposto
+#         print('Not all values are valid!')
+#
+# # test(a)
+#
+#
+#
+# def test(value):
+#     valid = [int(x) >= 10 for x in value.split(',') ]
+#     print(valid)
+#
+#     if (len(valid) != 10):
+#         return False
+#
+#     if (not all(valid)):
+#         return False
+#     return True
 
-a = '10, 4, 70, 80, 50, 100, 150, 20, 45, 88'
+matrice = [[1, 0, 3], [4, 1, 6]]
+
+num =33
+s = []
 
 
-def test1(value):
-    print(value)
-    arr = value.split(',')
-    print(arr)
-    integers = [int(x) for x in arr]
-    print(integers)
-    valid = [x >= 10 for x in integers] # retorna TRUE OU FALSE para cada iten do array
-    print(valid)
+for l in matrice:
+        for c in l:
+            if num != c:
+                print('No')
+        print('Yes')
 
-    if (all(valid)): # varifica se todos os itens do array sao True
-        print('All values are valid!')
-    if (not all(valid)): # oposto
-        print('Not all values are valid!')
+for i in range(0, len(matrice)):
+    for j in range(0, len(matrice)):
+        if matrice[i][j] == num:
+            position = i, j
+            s.append(position)
 
-# test(a)
-
-
-
-def test(value):
-    valid = [int(x) >= 10 for x in value.split(',') ]
-    print(valid)
-
-    if (len(valid) != 10):
-        return False
-
-    if (not all(valid)):
-        return False
-    return True
+print(s)
