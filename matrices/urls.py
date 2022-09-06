ec579a93c1caacbe9523ca1030f78d59b5b25762
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-09-06 11:37:08 +0100

    Matrice ex10 finished

--- matrices/urls.py
+++ matrices/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home, matrice_ex1
+from .views import home, matrice_ex1, matrice_ex10
 
 
 urlpatterns = [
     path('', home, name='home_matrices'),
     path('matrice_ex1', matrice_ex1, name='matrice_ex1_x'),
+    path('matrice_ex10', matrice_ex10, name='matrice_ex10_x'),
 ]
--- matrices/views.py
+++ matrices/views.py
@@ -35,3 +35,26 @@ def matrice_ex1(request):
                 position = i, j
 
     return render(request, 'matrices/matrice_ex1.html', {'matrices': matrices, 'maior': maior, 'position': position})
+
+def matrice_ex10(request):
+    mat = [[10, 2, 3],
+           [4, 5, 6],
+           [8, 8, 9]]
+
+    highest = 0
+    summ_sec = 0
+    diag_pri = []
+    diag_sec = []
+
+    n = len(mat)
+    for i in range(len(mat)):
+        diag_pri.append(mat[i][i])
+        if mat[i][i] > highest:
+            highest = mat[i][i]
+        for j in range(len(mat[i])):
+            if i + j == (n - 1):
+                diag_sec.append(mat[i][j])
+                summ_sec += mat[i][j]
+    return render(request, 'matrices/matrice_ex10.html', {'mat': mat,
+                                                          'highest': highest,
+                                                          'summ_sec': summ_sec})
