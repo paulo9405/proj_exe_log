ea71eb7ffa6efd0be83f78373b8ada2e92665f53
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-09-09 09:40:15 +0100

    functions ex1 fished

--- functions/views.py
+++ functions/views.py
@@ -6,4 +6,18 @@ def home(request):
 
 
 def functions_ex1(request):
-    return render(request, 'functions/functions_ex1.html')
+    if request.method == 'POST':
+        value = request.POST.get('value')
+
+
+        def calculate_volume(r):
+            r = r
+            pi = 3.14
+            v = 4 * (r ** 3)
+            v = (v / 3) * pi
+            return v
+
+    volume = calculate_volume(int(value))
+    volume = round(volume, 2)
+
+    return render(request, 'functions/functions_ex1.html', {'volume': volume, 'radius': value})
