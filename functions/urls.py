a2bcca1462a8020fff4db8a10e506a61989bc070
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-10-07 18:16:26 +0100

    functions ex8 finished

--- functions/urls.py
+++ functions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, functions_ex1, functions_ex6
+from .views import home, functions_ex1, functions_ex6, functions_ex8
 
 
 urlpatterns = [
     path('', home, name='home_functions'),
     path('functions_ex1', functions_ex1, name='functions_ex1_x'),
     path('functions_ex6', functions_ex6, name='functions_ex6_x'),
+    path('functions_ex8', functions_ex8, name='functions_ex8_x'),
 ]
--- functions/views.py
+++ functions/views.py
@@ -23,3 +23,32 @@ def functions_ex6(request):
         tot_day = calc(int(age))
 
     return render(request, 'functions/functions_ex6.html', {'age': age, 'tot_day': tot_day})
+
+
+def functions_ex8(request):
+    result = None
+    value = None
+    if request.method == 'POST':
+        value = request.POST.get('value')
+
+        def id_categ(a):
+            if a >= 5 and a <= 7:
+                return 'Infantil A'
+
+            if a >= 8 and a <= 10:
+                return 'Infantil B'
+
+            if a >= 11 and a <= 13:
+                return 'Juvenil A'
+
+            if a >= 14 and a <= 17:
+                return 'Juvenil A'
+
+            if a > 17:
+                return 'Juvenil B'
+
+            else:
+                return 'Idade permitida a partir de 5 anos.'
+
+        result = id_categ(int(value))
+    return render(request, 'functions/functions_ex8.html', {'result': result, 'value': value})
