6fe6c964a83fef47e23ecdcf9b9899c3f798a858
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-10-10 10:56:08 +0100

    functions ex11 finished

--- functions/urls.py
+++ functions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, functions_ex1, functions_ex6
+from .views import home, functions_ex1, functions_ex6, functions_ex11
 
 
 urlpatterns = [
     path('', home, name='home_functions'),
     path('functions_ex1', functions_ex1, name='functions_ex1_x'),
     path('functions_ex6', functions_ex6, name='functions_ex6_x'),
+    path('functions_ex11', functions_ex11, name='functions_ex11_x'),
 ]
--- functions/views.py
+++ functions/views.py
@@ -23,3 +23,35 @@ def functions_ex6(request):
         tot_day = calc(int(age))
 
     return render(request, 'functions/functions_ex6.html', {'age': age, 'tot_day': tot_day})
+
+def functions_ex11(request):
+    note = []
+    media = 0
+    media_sum = None
+    media_total = None
+
+    if request.method == 'POST':
+        notes = request.POST.get('value').split(',')
+
+        for i in notes:
+            note.append(int(i))
+            media += int(i)
+
+        def media_studant(a):
+
+            if a >= 0 and a <= 4.9:
+                return 'D'
+
+            if a >= 5 and a <= 6.9:
+                return 'C'
+
+            if a >= 7 and a <= 8.9:
+                return 'B'
+
+            if a >= 9 and a <= 10:
+                return 'A'
+
+        media_sum = media / len(note)
+        media_sum = round(media_sum, 2)
+        media_total = media_studant(media_sum)
+    return render(request, 'functions/functions_ex11.html', {'media_sum': media_sum, 'media_total': media_total})
