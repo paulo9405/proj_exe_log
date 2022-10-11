6baf0df52ba9071f87b90929c924fc20b86c68b3
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-10-11 17:58:20 +0100

    funxtions ex12 fnished

--- functions/urls.py
+++ functions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, functions_ex1, functions_ex6
+from .views import home, functions_ex1, functions_ex6, functions_ex12
 
 
 urlpatterns = [
     path('', home, name='home_functions'),
     path('functions_ex1', functions_ex1, name='functions_ex1_x'),
     path('functions_ex6', functions_ex6, name='functions_ex6_x'),
+    path('functions_ex12', functions_ex12, name='functions_ex12_x'),
 ]
--- functions/views.py
+++ functions/views.py
@@ -23,3 +23,27 @@ def functions_ex6(request):
         tot_day = calc(int(age))
 
     return render(request, 'functions/functions_ex6.html', {'age': age, 'tot_day': tot_day})
+
+
+def functions_ex12(request):
+    res = None
+    if request.method == 'POST':
+        value1 = request.POST.get('value1').upper()
+        value2 = request.POST.get('value2')
+        value3 = request.POST.get('value3')
+
+        def weight_ideal(s, a, w):
+            s = value1
+            a = int(value2) / 100
+            w = float(value3)
+            if s == 'M':
+                ideal = (w * a) - 58
+                return 'ideal weight', ideal
+
+            else:
+                if s == 'F':
+                    ideal = (w * a) - 44.7
+                    return 'ideal weight', ideal
+
+        res = weight_ideal(value1, value2, value3)
+    return render(request, 'functions/functions_ex12.html', {'res': res})
