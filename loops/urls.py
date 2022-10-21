6a8f4569383e76d7f9ccdb371816f0aa2aaf76b8
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-10-21 10:58:16 +0100

    loops_ex1 finished

--- loops/urls.py
+++ loops/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import (
+    home,
+    loops_ex1,
+)
 
 
 urlpatterns = [
     path('', home, name='home_loops'),
+    path('loops_ex1', loops_ex1, name='loops_ex1_x'),
 ]
