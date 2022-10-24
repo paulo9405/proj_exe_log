e7f16d8276fd38bccecbb79c008a244bacdc717a
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-10-24 23:36:34 +0100

    using just one function with all details

--- loops/views.py
+++ loops/views.py
@@ -8,31 +8,38 @@ def loops_ex7(request):
     value = 0
     total_decont = 0
     unidades_de_cem = 0
+    total = 0
     if request.method == 'POST':
         value = request.POST.get('value')
-
-    def desconto(total):
         total = int(value)
-
-        if total < 500:
-            valor_final = total
-            return valor_final
         resto = total - 500
-        unidades_de_cem = resto / 100
-        while unidades_de_cem <= 25:
-            total_decont = (total / 100) * unidades_de_cem
-            valor_final = total - total_decont
-            return valor_final
-        else:
-            total_decont = total / 4
-            valor_final = total - total_decont
-        return valor_final
+        unidades_de_100 = resto / 100
+        unidades_de_cem = int(unidades_de_100)
 
-    unidades_de_cem = unidades_de_cem
-    total_decont = total_decont
-    v_final = desconto(int(value))
+    if total < 500:
+        v_final = int(value)
+        return render(request, 'loops/loops_ex7.html', {'v_final': v_final,
+                                                        'total_decont': total_decont,
+                                                        'value': value}, )
 
-    return render(request, 'loops/loops_ex7.html', {'v_final': v_final,
-                                                    'total_decont': total_decont,
-                                                    'unidades_de_cem': unidades_de_cem,
-                                                    'value': value},)
+
+    while unidades_de_cem <= 25:
+        total_decont = (total / 100)
+        total_decont = int(total_decont) * unidades_de_cem
+        v_final = total - total_decont
+        return render(request, 'loops/loops_ex7.html', {'v_final': v_final,
+                                                        'total_decont': total_decont,
+                                                        'unidades_de_cem': unidades_de_cem,
+                                                        'value': value}, )
+
+    else:
+        total_decont = total / 4
+        total_decont = int(total_decont)
+        unidades_de_cem = 25
+        v_final = total - total_decont
+        return render(request, 'loops/loops_ex7.html', {'v_final': v_final,
+                                                        'total_decont': total_decont,
+                                                        'unidades_de_cem': unidades_de_cem,
+                                                        'value': value}, )
+
+
