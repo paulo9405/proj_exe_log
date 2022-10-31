1fbd5ae9fe3ff17131d35da53602089b5bbd2e69
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-10-31 14:09:34 +0000

    split in 3 functions

--- functions/views.py
+++ functions/views.py
@@ -27,21 +27,30 @@ def functions_ex2(request):
                 error = 'Plese write just one value A, P or H.'
                 return render(request, 'functions/functions_ex2.html', {'error': error})
 
-            def media_student(v1):
-                v1 = value1
-                media = 0
-                if value2 == 'A':
+            if value2 == 'A':
+                def media_student(v1):
+                    v1 = value1
+                    media = 0
                     for i in v1:
                         media += int(i) / len(v1)
                         msg = 'student media'
                     return media, msg
 
-                elif value2 == 'P':
+                m = media_student(value1)
+
+            if value2 == 'P':
+                def media_ponderada(v1):
+                    v1 = value1
                     media = ((int(v1[0]) * 5) + (int(v1[1]) * 3) + (int(v1[2]) * 2)) / (5 + 2 + 3)
+                    media = round(media, 2)
                     msg = 'media ponderada'
                     return media, msg
+                m = media_ponderada(value1)
 
-                elif value2 == 'H':
+            if value2 == 'H':
+                def media_harmonica(v1):
+                    v1 = value1
+
                     def mcm(x, y, i):
                         z = max(x, y, i)
                         while True:
@@ -51,10 +60,10 @@ def functions_ex2(request):
 
                     mmc = mcm(int(v1[0]), int(v1[1]), int(v1[2]))
                     r = (mmc / int(v1[0]) * 1) + (mmc / int(v1[1]) * 1) + (mmc / int(v1[2]) * 1)
-                    r = (3 * mmc) / r
+                    r = round((3 * mmc) / r, 2)
                     msg = 'media harmonizada'
                     return r, msg
 
-            m = media_student(value1)
+                m = media_harmonica(value1)
 
     return render(request, 'functions/functions_ex2.html', {'m': m, 'value1': value1})
