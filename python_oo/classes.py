3ff2b4c3cdcb9f58d3106131f4138133f71be28b
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-12-05 01:07:05 +0000

    ex9 poo finished

--- python_oo/classes.py
+++ python_oo/classes.py
@@ -17,3 +17,32 @@ class Contador(object):
     def __del__(self):
         self.total = 0
         return self.total
+
+
+class Person(object):
+    def __init__(self, name, dad, mom):
+        self.name = name
+        self.dad = dad
+        self.mom = mom
+
+    def is_same(self, other_person):
+        if self.name == other_person.name and self.mom == other_person.mom:
+            return True
+
+    def is_sibling(self, other_person):
+        return self.dad == other_person.dad and self.mom == other_person.mom
+
+    def is_predecessor(self, other_person):
+        if self.is_same(other_person):
+            return False
+
+        if self.dad == other_person or self.mom == other_person:
+            return True
+
+        if self.dad is not None and self.dad.is_predecessor(other_person):
+            return True
+
+        if self.mom is not None and self.mom.is_predecessor(other_person):
+            return True
+
+        return False
